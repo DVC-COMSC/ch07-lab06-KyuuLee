bf77bd863d361067129963c52a3bd4719fb693aa
Kyuu <69904775+KyuuLee@users.noreply.github.com>
2022-09-28 13:35:53 -0700

    Update main.py

--- main.py
+++ main.py
@@ -3,11 +3,14 @@ inputvalues = input('Enter all elements values: ')
 numbers1 = inputvalues.split() 
 for i in range(len(numbers1)):
 	numbers1[i] = int(numbers1[i]) 
-# The following line is the same as the for-loop
-# numbers1 = list(map(int, numbers))
-
 print ("The original list: ", numbers1)
 
-# ******************************
-# Make your Code
-# ******************************
+numbers2 = []
+for i in range(len(numbers1)):
+	numbers2.append(numbers1.pop())
+
+
+print ("The original list: ", numbers1)
+print ("The reverse list: ", numbers2)
+
+
